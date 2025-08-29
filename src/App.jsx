c3fdc4c448152bc3bdff47292b2fe1371d63c0d5
Dilu Tharushika <dilutharushikagamage2@gmail.com>
2025-08-29 18:47:41 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Main Sections
 import Navbar from "./components/navbar/Navbar.jsx";
@@ -13,12 +13,11 @@ import Footer from "./components/footer/footer.jsx";
 
 // Project Detail Pages
 import Recipe from "./components/projects/recipe/recipe.jsx";
-import Pharmacy from "./components/projects/pharmacy/pharmacy.jsx" 
+import Pharmacy from "./components/projects/pharmacy/pharmacy.jsx";
 import Game from "./components/projects/game/game.jsx";
 import Cafe from "./components/projects/cafe/cafe.jsx";
 import Filmhall from "./components/projects/filmhall/filmhall.jsx";
 import Todo from "./components/projects/todo/todo.jsx";
-// Later you can add Pharmacy, Games, Cafe, Film, ToDo, etc.
 
 const App = () => {
   return (
@@ -43,12 +42,11 @@ const App = () => {
 
         {/* Project Detail Pages */}
         <Route path="/recipe" element={<Recipe />} />
-        <Route path="/pharmacy" element={<Pharmacy/>}/>
-        <Route path="/game" element={<Game/>}/>
-        <Route path="/cafe" element={<Cafe/>}/>
-        <Route path="/film" element={<Filmhall/>}/>
-        <Route path="/todo" element={<Todo/>}/>
-        {/* Example: <Route path="/pharmacy" element={<Pharmacy />} /> */}
+        <Route path="/pharmacy" element={<Pharmacy />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/cafe" element={<Cafe />} />
+        <Route path="/film" element={<Filmhall />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </Router>
   );
